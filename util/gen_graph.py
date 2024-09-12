25fcaa4841977ad308a190639862299ab5b4360a
yinuo <yj2589@columbia.edu>
2024-09-12 16:30:01 -0400

    Minor updates - environment

--- util/gen_graph.py
+++ util/gen_graph.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-import cupy as cp
+# import cupy as cp
 import torch
 import networkx as nx
 
@@ -14,6 +14,7 @@ from torch_geometric import utils as pyg_utils
 from scipy.sparse import csr_matrix
 
 
+"""
 def get_centroids(mask):
     mempool = cp.get_default_memory_pool()
 
@@ -28,7 +29,7 @@ def get_centroids(mask):
         mempool.free_all_blocks()    
 
     return np.array(coords)
-
+"""
 
 def get_coords_from_graph(G, node_list=None):
     try:
