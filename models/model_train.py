Fix masked attention gradient issues

models/model_train.py
@@ -58,14 +58,17 @@ def train_vgae(
 
         for data in dataloader:
             s = data.s.to(device).float()
-            edge_index = data.edge_index.to(device)
+            edge_index = data.edge_index.contiguous().to(device)
 
             if isinstance(model, vgae.MultiscaleVGAE):
                 # VGAE with multi-scale graphs (X -> Z -> Y)
                 x = data.x.to(device).float()
                 y = data.y.to(device).float()
                 pooling_cluster = data.cluster.to(device)
-                loss = svi.step(x, y, s, edge_index, pooling_cluster)
+
+                # DEBUG NaNs:
+                with torch.autograd.detect_anomaly():
+                    loss = svi.step(x, y, s, edge_index, pooling_cluster)
                 n_obs += y.size(0)
             else:
                 # VGAE with interpolated (same-dim) graphs (U -> Z -> X)
@@ -86,7 +89,7 @@ def train_vgae(
             with torch.no_grad():
                 for data in val_dataloader:
                     s = data.s.to(device).float()
-                    edge_index = data.edge_index.to(device)
+                    edge_index = data.edge_index.contiguous().to(device)
                     if isinstance(model, vgae.MultiscaleVGAE):
                         x = data.x.to(device).float()
                         y = data.y.to(device).float()

models/module.py
@@ -155,11 +155,10 @@ class SurjectiveAttention(nn.Module):
         K = self.key_proj(X, edge_index)   # dim: [N, E]
 
         # Attention scores for each pair of surjective cell-pixel map
-        scores = torch.matmul(Q, K.transpose(0, 1)) / (self.embed_dim**0.5)  # dim: [L, N)
-        scores = scores.masked_fill(mask == 0, float('-inf'))
-        attn_weights = F.softmax(scores, dim=-1)
-
-        return scores, attn_weights
+        scores = Q @ K.T / (self.embed_dim**0.5)  # dim: [L, N]
+        exp_scores = torch.exp(scores)*mask
+        attn_weights = exp_scores / (exp_scores.sum(dim=-1, keepdim=True)+EPS)
+        return scores*mask, attn_weights
     
 
 # --------------------------------

models/vgae.py
@@ -310,7 +310,7 @@ class MultiscaleVGAE(VGAE):
 
         # Normalize Xenium counts
         x = self.__lognorm(x) 
-        edge_index = edge_index.contiguous()
+        edge_index = edge_index
 
         with pyro.plate("batch", y.size(0)), poutine.scale(scale=self.configs.beta):
             # Cell-level stats
@@ -328,7 +328,7 @@ class MultiscaleVGAE(VGAE):
 
             z_dist = dist.Normal(z_mu_pooled, torch.exp(z_logvar_pooled/2))
             z = pyro.sample("z", z_dist.to_event(1))
-
+            
             y_mu, y_logvar = self.decode(z, s, edge_index_pooled)
             normal_dist = dist.Normal(y_mu, torch.exp(y_logvar/2))
             pyro.sample("y", normal_dist.to_event(1), obs=y)
@@ -338,7 +338,7 @@ class MultiscaleVGAE(VGAE):
         
         # Normalize Xenium counts
         x = self.__lognorm(x) 
-        edge_index = edge_index.contiguous()
+        edge_index = edge_index
 
         # Pooled pixel-level graph
         edge_index_pooled = avg_pool(
@@ -357,7 +357,7 @@ class MultiscaleVGAE(VGAE):
 
     def get_z(self, x, y, s, edge_index, pooling_cluster):
         x = self.__lognorm(x)
-        edge_index = edge_index.contiguous()
+        edge_index = edge_index
         
         edge_index_pooled = avg_pool(
             cluster=pooling_cluster,
@@ -483,7 +483,7 @@ class MultiscaleVGAE(VGAE):
         return mask
 
     def __lognorm(self, x):
-        l = x.sum(axis=-1, keepdim=True)
-        x = x / l * l.median()
+        l = x.sum(axis=-1, keepdim=True) + EPS
+        x = x / l * l.median() 
         return torch.log1p(x)  
 